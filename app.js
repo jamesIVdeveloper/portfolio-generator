d01d4f53426dfb5a80c76dfd933e1af849b561c8
James Murphy <jamesivdeveloper@gmail.com>
2022-02-12 12:00:30 -0600

    generate webpage

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete!  Check out index.html to see the output!");
+});
+
+/*
 console.log(profileDataArgs);
 
 const printProfileData = (profileDataArr) => {
@@ -12,3 +25,4 @@ const printProfileData = (profileDataArr) => {
 };
 
 printProfileData(profileDataArgs);
+*/
