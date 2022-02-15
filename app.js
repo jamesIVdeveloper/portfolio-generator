b5d5af37fb71cf719882fb4ef812a8ab9db2ab50
James Murphy <jamesivdeveloper@gmail.com>
2022-02-15 12:49:07 -0600

    add inquirer

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+/*
 const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const generatePage = require("./src/page-template");
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
+fs.writeFile("./index.html", pageHTML, (err) => {
   if (err) throw err;
 
   console.log("Portfolio complete!  Check out index.html to see the output!");
 });
-
-/*
-console.log(profileDataArgs);
-
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log("================");
-
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
 */
